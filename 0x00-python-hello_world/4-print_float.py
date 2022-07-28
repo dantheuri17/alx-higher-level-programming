2f8ae28214ea876c49e56e87484377f9388332cf
Daniel Theuri <dantheuri17@gmail.com>
2022-07-28 23:54:58 +0000

    finished up to 8

---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,3 @@
+#!/usr/python3
+num = 3.14159
+print(f"Float: {num:.2f}")
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/pyton
+str1 = "Holberton"
+str2 = "School"
+st1 = str1 + " " + str2
+printf"Welcome to {str1}!")
