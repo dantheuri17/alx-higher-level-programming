94d6345a1a43d9297c34723c40357067a17b67b4
Daniel Theuri <dantheuri17@gmail.com~>
2022-08-23 21:58:41 +0300

    addin

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
+
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
-        return
-    n = []
-    for new in a_dictionary:
-        n.append(new)
-    return max(n)
+    if a_dictionary is None:
+        return None
+    maxval = 0
+    maxkey = None
+    for k, v in a_dictionary.items():
+        if v > maxval:
+            maxval = v
+            maxval = k
+    return maxkey
