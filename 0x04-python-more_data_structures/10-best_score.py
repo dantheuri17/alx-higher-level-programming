f9e4c8c1bfb39805233754ced2ba0b24fbd065e4
Daniel Theuri <dantheuri17@gmail.com~>
2022-08-23 22:05:59 +0300

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-
-
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    maxval = 0
-    maxkey = None
-    for k, v in a_dictionary.items():
-        if v > maxval:
-            maxval = v
-            maxval = k
-    return maxkey
+    if a_dictionary is None or a_dictionary == {}:
+        return
+    n = []
+    for new in a_dictionary:
+        n.append(new)
+    return max(n)
