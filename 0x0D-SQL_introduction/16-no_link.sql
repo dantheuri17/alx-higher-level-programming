b8a482f6db465cda6970527468ac2eb88b5e141c
Daniel Theuri <dantheuri17@gmail.com~>
2022-09-07 05:29:07 +0300

    second_table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- fetch all scores and names where name field is NOT empty, then order it by largest to smallest
-SELECT score, name FROM first_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
